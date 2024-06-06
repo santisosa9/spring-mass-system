6c74f6e3345c903bcd0086981666851ab889256d
Santiago Sosa <santiagososamoressi@gmail.com>
2024-06-06 11:50:16 -0300

    modularization

--- src/main.py
+++ src/main.py
@@ -11,20 +11,19 @@ v0 = 0.0   # velocidad inicial
 t_max = 10 # tiempo de simulacion 
 h = 0.01   # paso de integracion
 
+# Variables para almacenar los resultados
+t_values = np.arange(0, t_max, h)
+x_values = []
+v_values = []
+
+# v'(t)
 def acceleration(x, v):
   return -k/m * x - b/m * v + 1/m * f
 
-def main():
-  # Variables para almacenar los resultados
-  t_values = np.arange(0, t_max, h)
-  x_values = []
-  v_values = []
-
-  # Inicializar las condiciones iniciales
-  x = x0
-  v = v0
-
-  # Bucle de integración numérica (Euler)
+# Metodo de integración numérica (Euler)
+def euler(x, v):
+  x_values.clear
+  v_values.clear
   for t in t_values:
     x_values.append(x)
     v_values.append(v)
@@ -32,8 +31,9 @@ def main():
     v_aux = v
     v += h * a
     x += h * v_aux
-
-  # Graficar los resultados
+  
+# Graficar los resultados
+def plot():
   plt.plot(t_values, x_values, label='Posición (m)')
   plt.plot(t_values, v_values, label='Velocidad (m/s)')
   plt.xlabel('Tiempo (s)')
@@ -41,5 +41,14 @@ def main():
   plt.title('Simulación del Modelo Masa-Resorte')
   plt.savefig('img/mygraph.png')
 
+def main():
+  # Inicializar las condiciones iniciales
+  x = x0
+  v = x0
+  
+  euler(x,v)
+  
+  plot()
+  
 if __name__ == '__main__':
   main()
